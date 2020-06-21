1d859aa97ab786101c26ecc37b9d9f0830f6422a
su10 <1687366+su10@users.noreply.github.com>
2020-06-21 19:51:09 +0900

    SceneAssetしかセットできないAssetReferenceクラス追加

---
+++ Assets/Plugins/Zenject/Source/AssetReferenceSceneAsset.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine.AddressableAssets;
+
+[Serializable]
+public class AssetReferenceSceneAsset : AssetReferenceT<
+#if UNITY_EDITOR
+    UnityEditor.SceneAsset
+#else
+    UnityEngine.Object
+#endif
+>
+{
+    public AssetReferenceSceneAsset(string guid) : base(guid)
+    {
+    }
+}
